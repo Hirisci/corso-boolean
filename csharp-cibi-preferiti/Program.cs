130fe9c86a15dc515e504c73b2a518915809f14b
Hirisci <www.alan92@icloud.com>
2022-09-15 15:53:24 +0200

    Finito con bonus ed eccezione nel caso di pari

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -2,28 +2,15 @@
 using System;
 
 
-//Esercizio di oggi sugli Array:
-//Miei cibi preferiti
-//Nome repo: csharp - cibi - preferiti
-//Create un progetto App Console CibiPreferiti con VS2022. Nel programma inizializzate un array con la classifica dei vostri cibi preferiti (minimo 5, massimo 10 elementi). L’array deve
-//essere già inizializzato nel programma, e i vostri cibi preferiti non vanno chiesti all’utente tramite input.
-//Una volta dichiarato l’array di cibi preferiti, stampate a video le seguenti informazioni:
-//La lunghezza della classifica
-//La vostra classifica (dunque stampare l’intero array in ordine indicando la posizione in classifica)
-//Il vostro cibo top (prima posizione della classifica)
-//Il vostro cibo preferito ma non troppo (ultima posizione della classifica)
-//Le modalità di consegna sono le solite: repo GitHub, commit e push
-//BONUS Stampate a video anche il cibo di mezza classifica, cioè che si trova nella posizione mediana. Attenzione: gestire anche il caso se aveste una classifica con un numero di elementi pari. In questo caso vanno stampati i 2 elementi in centro alla vostra classifica.
-//Buon pomeriggio e buon lavoro!
-
-
-
-
+///Una volta dichiarato l’array di cibi preferiti
 String[] food = { "Sushi", "Pizza", "Lasagne", "Pasta al Forno", "Carbonara" };
 
+//La lunghezza della classifica
 Console.WriteLine("Lungezza lista: " + food.Length);
 Console.WriteLine();
+
 Console.WriteLine("Classifica cibi preferiti:");
+//La vostra classifica (dunque stampare l’intero array in ordine indicando la posizione in classifica)
 for (int i = 0; i < food.Length; i++)
 {
     Console.WriteLine(i+") " + food[i]);
@@ -31,5 +18,19 @@ for (int i = 0; i < food.Length; i++)
 
 Console.WriteLine();
 
+//Il vostro cibo top (prima posizione della classifica)
 Console.WriteLine("Cibo Top: "+ food[0]);
-Console.WriteLine("Cobo preferito ma non troppo: " + food[food.Length-1]);
+//Il vostro cibo preferito ma non troppo (ultima posizione della classifica)
+Console.WriteLine("Cibo preferito ma non troppo: " + food[food.Length-1]);
+//BONUS Stampate a video anche il cibo di mezza classifica, cioè che si trova nella posizione mediana. 
+Console.WriteLine("Cibo intermedio: " + food[food.Length/2]);
+
+
+//Attenzione: gestire anche il caso se aveste una classifica con un numero di elementi pari. In questo caso vanno stampati i 2 elementi in centro alla vostra classifica.
+String[] foodEven = { "Sushi", "Pizza", "Lasagne", "Pasta al Forno", "Carbonara", "Spagetti con le vongole" };
+if (foodEven.Length % 2 == 0)
+{
+    Console.WriteLine("Cibo intermedio: " + food[food.Length / 2]); 
+    Console.WriteLine("Cibo intermedio: " + food[(food.Length / 2)+1]);
+}
+ 
