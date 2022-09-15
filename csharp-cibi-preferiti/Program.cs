99d7f260804964bccfacfab89257e33080cae3f1
Hirisci <www.alan92@icloud.com>
2022-09-15 15:31:14 +0200

    Esercizio Finito

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,3 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//prova
+using System;
+
+
+//Esercizio di oggi sugli Array:
+//Miei cibi preferiti
+//Nome repo: csharp - cibi - preferiti
+//Create un progetto App Console CibiPreferiti con VS2022. Nel programma inizializzate un array con la classifica dei vostri cibi preferiti (minimo 5, massimo 10 elementi). L’array deve
+//essere già inizializzato nel programma, e i vostri cibi preferiti non vanno chiesti all’utente tramite input.
+//Una volta dichiarato l’array di cibi preferiti, stampate a video le seguenti informazioni:
+//La lunghezza della classifica
+//La vostra classifica (dunque stampare l’intero array in ordine indicando la posizione in classifica)
+//Il vostro cibo top (prima posizione della classifica)
+//Il vostro cibo preferito ma non troppo (ultima posizione della classifica)
+//Le modalità di consegna sono le solite: repo GitHub, commit e push
+//BONUS Stampate a video anche il cibo di mezza classifica, cioè che si trova nella posizione mediana. Attenzione: gestire anche il caso se aveste una classifica con un numero di elementi pari. In questo caso vanno stampati i 2 elementi in centro alla vostra classifica.
+//Buon pomeriggio e buon lavoro!
+
+
+
+
+String[] food = { "Sushi", "Pizza", "Lasagne", "Pasta al Forno", "Carbonara" };
+
+Console.WriteLine("Lungezza lista: " + food.Length);
+Console.WriteLine();
+Console.WriteLine("Classifica cibi preferiti:");
+for (int i = 0; i < food.Length; i++)
+{
+    Console.WriteLine(food[i] +") " + food[i]);
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Cibo Top: "+ food[0]);
+Console.WriteLine("Cobo preferito ma non troppo: " + food[food.Length-1]);
