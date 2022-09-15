b93d38ddb69b87af5201a02f5e61a7fd5000a682
Hirisci <www.alan92@icloud.com>
2022-09-15 16:12:51 +0200

    upgrade code

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -6,31 +6,34 @@ using System;
 String[] food = { "Sushi", "Pizza", "Lasagne", "Pasta al Forno", "Carbonara" };
 
 //La lunghezza della classifica
-Console.WriteLine("Lungezza lista: " + food.Length);
+Console.WriteLine($"Lungezza lista: {food.Length}" );
 Console.WriteLine();
 
 Console.WriteLine("Classifica cibi preferiti:");
 //La vostra classifica (dunque stampare l’intero array in ordine indicando la posizione in classifica)
 for (int i = 0; i < food.Length; i++)
 {
-    Console.WriteLine(i+") " + food[i]);
+    Console.WriteLine($"{i+1}) + {food[i]}");
 }
 
 Console.WriteLine();
 
 //Il vostro cibo top (prima posizione della classifica)
-Console.WriteLine("Cibo Top: "+ food[0]);
+Console.WriteLine($"Cibo Top: {food[0]}");
 //Il vostro cibo preferito ma non troppo (ultima posizione della classifica)
-Console.WriteLine("Cibo preferito ma non troppo: " + food[food.Length-1]);
+Console.WriteLine($"Cibo preferito ma non troppo: {food[food.Length-1]}");
 //BONUS Stampate a video anche il cibo di mezza classifica, cioè che si trova nella posizione mediana. 
-Console.WriteLine("Cibo intermedio: " + food[food.Length/2]);
+Console.WriteLine($"Cibo intermedio: {food[food.Length/2]}");
 
+Console.WriteLine("Bonus con Array Pari");
 
+Console.WriteLine();
+Console.WriteLine();
 //Attenzione: gestire anche il caso se aveste una classifica con un numero di elementi pari. In questo caso vanno stampati i 2 elementi in centro alla vostra classifica.
 String[] foodEven = { "Sushi", "Pizza", "Lasagne", "Pasta al Forno", "Carbonara", "Spagetti con le vongole" };
 if (foodEven.Length % 2 == 0)
 {
-    Console.WriteLine("Cibo intermedio: " + food[food.Length / 2]); 
-    Console.WriteLine("Cibo intermedio: " + food[(food.Length / 2)+1]);
+    Console.WriteLine($"Cibo intermedio: {food[food.Length / 2]}"); 
+    Console.WriteLine($"Cibo intermedio: {food[(food.Length / 2)+1]}");
 }
  
